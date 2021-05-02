df401bd799587079a641692ba362e1adfc254866
BuildTools <unconfigured@null.spigotmc.org>
2021-05-02 12:09:24 +0200

    Add Read-Manipulation

--- example_4_simple_manipulation/script.js
+++ example_4_simple_manipulation/script.js
@@ -1,4 +1,10 @@
 function change_color(){
     var box_element = document.getElementById("box")
+    
     box_element.style.backgroundColor = "red"
+    box_element.style.width = "400px"
+}
+function show_input(){
+  var show = document.getElementById("eingabefeld")
+  alert(show.value)
 }
